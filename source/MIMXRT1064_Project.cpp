2a13b52e8b3ed19069fd4314fa43a5bd459187e5
yerih <yerih7tup@gmail.com>
2024-11-09 08:46:15 -0400

    UART is working
    printf is working.

--- source/MIMXRT1064_Project.cpp
+++ source/MIMXRT1064_Project.cpp
@@ -40,7 +40,9 @@ int main(void) {
     volatile static int i = 0 ;
     /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
+    	PRINTF("Hello World\r\n");
         i++ ;
+
         /* 'Dummy' NOP to allow source level single stepping of
             tight while() loop */
         __asm volatile ("nop");
